734c1483f4d27beadd165821d5a0c769e38237c9
1unarzDev <lunarzDev@outlook.com>
2024-12-28 21:30:20 -0600

    Created base motor turn script, min and max pulse still needed

--- firmware/PCA9685/src/main.cpp
+++ firmware/PCA9685/src/main.cpp
@@ -3,33 +3,25 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 
-Adafruit_PWMServoDriver board1 = Adafruit_PWMServoDriver(0x40); // PCA9685 default I2C address
+Adafruit_PWMServoDriver pca9685 = Adafruit_PWMServoDriver(0x40); // PCA9685 default I2C address
 
+// Modify the widths below for the pulses to map properly depending on the servo
 #define SERVOMIN 125  // Minimum pulse length
 #define SERVOMAX 625  // Maximum pulse length
 
 // Map angle to pulse width
 int angleToPulse(int ang) {
     int pulse = map(ang, 0, 180, SERVOMIN, SERVOMAX);
-    Serial.print("Angle: ");
-    Serial.print(ang);
-    Serial.print(" pulse: ");
-    Serial.println(pulse);
     return pulse;
 }
 
-void setup() {
-    Serial.begin(9600);
-    Serial.println("16-channel Servo Test!");
-    board1.begin();
-    board1.setPWMFreq(60); // Analog servos run at ~60 Hz updates
+void loop(int motor) {
+    pca9685.setPWM(motor, 0, angleToPulse((rand() - RAND_MAX / 2) * 2 / RAND_MAX ));
 }
 
-void loop() {
-    // Sweep angles from 0 to 180 for channels 0-7
-    for (int angle = 0; angle <= 180; angle += 10) {
-        for (int i = 0; i < 15; i++) {
-            board1.setPWM(i, 0, angleToPulse(angle));
-        }
-    }
+void setup() {
+    Serial.begin(9600); // Adjust the baud rate depending on your board (also in the plaformio.ini)
+    pca9685.begin();
+    pca9685.setPWMFreq(60); // Analog servos run at ~60 Hz updates
 }
+
